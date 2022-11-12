4bda789a42f0df893374f2e980695e2dbe665597
LauricioX <developerwebmasterpro@gmail.com>
2022-11-11 21:40:37 -0300

    front

--- backend/router.js
+++ backend/router.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const express = require("express");
 const userModel = require("./database/models/users");
 const router = express.Router();
@@ -16,6 +17,57 @@ router.post("/user", (req, res) => {
       email: email,
     })
     .then((resolve) => {
+      res.sendStatus(200);
+    })
+    .catch((erro) => {
+      res.sendStatus(400);
+    });
+});
+
+//rescebendo dados do banco
+
+router.get("/users", (req, res) => {
+  userModel
+    .findAll()
+    .then((response) => {
+      res.json(response);
+      res.sendStatus(200);
+    })
+    .catch((erro) => {
+      console.log(erro);
+    });
+});
+
+//deletando dados
+router.delete("/user/:id", (req, res) => {
+  userModel
+    .destroy({ where: { id: req.params.id } })
+    .then((response) => {
+      res.sendStatus(200);
+    })
+    .catch((erro) => {
+      res.sendStatus(400);
+    });
+});
+
+//atualizando dados;
+
+router.put("/user/:id", (req, res) => {
+  let { nome, idade, email } = req.body;
+  userModel
+    .update(
+      {
+        nome: nome,
+        idade: idade,
+        email: email,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    )
+    .then((response) => {
       res.sendStatus(200);
     })
     .catch((erro) => {
